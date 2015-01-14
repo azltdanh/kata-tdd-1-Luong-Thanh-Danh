15a2d2d18a4726bed63c6d8f3bf8e8f4a20791f6
azltdanh <az.ltdanh@gmail.com>
2015-01-14 17:16:07 +0700

    Support different delimiters

--- src/kata-tdd-1-Luong-Thanh-Danh.js
+++ src/kata-tdd-1-Luong-Thanh-Danh.js
@@ -5,12 +5,24 @@ if(typeof StringCalculator == 'undefined') {
 StringCalculator.stringCalculator = {
 	add: function(numbers){
 		var result = 0;
+		var defaultDelimiters = ['\n',','];
 		
+		// return 0 for an empty string
 		if(0 === numbers.length)
 			return result;
-			
-		var inputs = numbers.split(/[\n,]+/);
 		
+		// get custom delimiter
+		if(numbers.indexOf('//') == 0){
+			var customDelimiter = numbers.substring(numbers.indexOf('//') + 2,numbers.indexOf('\n'));
+			numbers = numbers.substring(numbers.indexOf('\n') + 1);
+			defaultDelimiters.push(customDelimiter);
+		}
+		
+		// split to array of number
+		var regex = new RegExp( '[' + defaultDelimiters + ']+' , 'g');
+		var inputs = numbers.split(regex);
+		
+		// sum of numbers
 		for (var i = 0; i < inputs.length; i++) {
 			result += parseInt(inputs[i]);
 		}
--- test/kata-tdd-1-Luong-Thanh-Danh.test.js
+++ test/kata-tdd-1-Luong-Thanh-Danh.test.js
@@ -2,23 +2,47 @@ describe("String Calculator",function(){
 	
 	var testCalculator = StringCalculator.stringCalculator;
 	
+	// The method can take 0, 1 or 2 numbers, and will return their sum 
+	// (for an empty string it will return 0)
 	it("should return 0 for an empty string", function(){
 		expect(testCalculator.add("")).toEqual(0);
 	})
 	
+	// The method can take 0, 1 or 2 numbers, and will return their sum 
+	// (for an empty string it will return 0)
 	it("should return 1 for '1' string", function(){
 		expect(testCalculator.add("1")).toEqual(1);
 	})
 	
+	// The method can take 0, 1 or 2 numbers, and will return their sum 
+	// (for an empty string it will return 0)
 	it("should return 3 for '1,2' string", function(){
 		expect(testCalculator.add("1,2")).toEqual(3);
 	})
 	
+	// Allow the Add method to handle an unknown amount of numbers
 	it("should return 6 for '1,2,3' string", function(){
 		expect(testCalculator.add("1,2,3")).toEqual(6);
 	})
 	
+	// Allow the Add method to handle new lines between numbers (instead of commas). 
 	it("should return 6 for '1\\n2,3' string", function(){
 		expect(testCalculator.add("1\n2,3")).toEqual(6);
 	})
+	
+	// Support different delimiters 
+	it("should return 3 for '//;\\n1;2' string", function(){
+		expect(testCalculator.add("//;\n1;2")).toEqual(3);
+	})
+	
+	// Support different delimiters 
+	it("should return 6 for '//;\\n1;2,3' string", function(){
+		expect(testCalculator.add("//;\n1;2,3")).toEqual(6);
+	})
+	
+	// Support different delimiters 
+	it("should return 10 for '//;\\n1;2,3\n4' string", function(){
+		expect(testCalculator.add("//;\n1;2,3\n4")).toEqual(10);
+	})
+	
 });
