00c711cde3a29e656da8c0a8c10d31c0dccf653b
azltdanh <az.ltdanh@gmail.com>
2015-01-15 10:23:38 +0700

    Calling Add with a negative number will throw an exception “negatives not allowed” - and the negative that was passed. if there are multiple

--- src/kata-tdd-1-Luong-Thanh-Danh.js
+++ src/kata-tdd-1-Luong-Thanh-Danh.js
@@ -1,32 +1,38 @@
-if(typeof StringCalculator == 'undefined') {
+if (typeof StringCalculator == 'undefined') {
 	StringCalculator = {};
 }
 
 StringCalculator.stringCalculator = {
-	add: function(numbers){
+	add : function (numbers) {
 		var result = 0;
-		var defaultDelimiters = ['\n',','];
-		
+		var defaultDelimiters = ['\n', ','];
+
 		// return 0 for an empty string
-		if(0 === numbers.length)
+		if (0 === numbers.length)
 			return result;
-		
+
 		// get custom delimiter
-		if(numbers.indexOf('//') == 0){
-			var customDelimiter = numbers.substring(numbers.indexOf('//') + 2,numbers.indexOf('\n'));
+		if (numbers.indexOf('//') == 0) {
+			var customDelimiter = numbers.substring(numbers.indexOf('//') + 2, numbers.indexOf('\n'));
 			numbers = numbers.substring(numbers.indexOf('\n') + 1);
 			defaultDelimiters.push(customDelimiter);
 		}
-		
+
+		// catch negative numbers exception
+		var negativeNumbers = numbers.match(/-\d+/g);
+		if (negativeNumbers != null && negativeNumbers.length > 0) {
+			throw new Error("negatives not allowed: " + negativeNumbers);
+		}
+
 		// split to array of number
-		var regex = new RegExp( '[' + defaultDelimiters + ']+' , 'g');
+		var regex = new RegExp('[' + defaultDelimiters + ']+', 'g');
 		var inputs = numbers.split(regex);
-		
+
 		// sum of numbers
 		for (var i = 0; i < inputs.length; i++) {
 			result += parseInt(inputs[i]);
 		}
-		
+
 		return result;
 	}
 };
--- test/kata-tdd-1-Luong-Thanh-Danh.test.js
+++ test/kata-tdd-1-Luong-Thanh-Danh.test.js
@@ -1,48 +1,56 @@
-describe("String Calculator",function(){
-	
+describe("String Calculator", function () {
+
 	var testCalculator = StringCalculator.stringCalculator;
-	
-	// The method can take 0, 1 or 2 numbers, and will return their sum 
+
+	// The method can take 0, 1 or 2 numbers, and will return their sum
 	// (for an empty string it will return 0)
-	it("should return 0 for an empty string", function(){
+	it("should return 0 for an empty string", function () {
 		expect(testCalculator.add("")).toEqual(0);
 	})
-	
-	// The method can take 0, 1 or 2 numbers, and will return their sum 
+
+	// The method can take 0, 1 or 2 numbers, and will return their sum
 	// (for an empty string it will return 0)
-	it("should return 1 for '1' string", function(){
+	it("should return 1 for '1' string", function () {
 		expect(testCalculator.add("1")).toEqual(1);
 	})
-	
-	// The method can take 0, 1 or 2 numbers, and will return their sum 
+
+	// The method can take 0, 1 or 2 numbers, and will return their sum
 	// (for an empty string it will return 0)
-	it("should return 3 for '1,2' string", function(){
+	it("should return 3 for '1,2' string", function () {
 		expect(testCalculator.add("1,2")).toEqual(3);
 	})
-	
+
 	// Allow the Add method to handle an unknown amount of numbers
-	it("should return 6 for '1,2,3' string", function(){
+	it("should return 6 for '1,2,3' string", function () {
 		expect(testCalculator.add("1,2,3")).toEqual(6);
 	})
-	
-	// Allow the Add method to handle new lines between numbers (instead of commas). 
-	it("should return 6 for '1\\n2,3' string", function(){
+
+	// Allow the Add method to handle new lines between numbers (instead of commas).
+	it("should return 6 for '1\\n2,3' string", function () {
 		expect(testCalculator.add("1\n2,3")).toEqual(6);
 	})
-	
-	// Support different delimiters 
-	it("should return 3 for '//;\\n1;2' string", function(){
+
+	// Support different delimiters
+	it("should return 3 for '//;\\n1;2' string", function () {
 		expect(testCalculator.add("//;\n1;2")).toEqual(3);
 	})
-	
-	// Support different delimiters 
-	it("should return 6 for '//;\\n1;2,3' string", function(){
+
+	// Support different delimiters
+	it("should return 6 for '//;\\n1;2,3' string", function () {
 		expect(testCalculator.add("//;\n1;2,3")).toEqual(6);
 	})
-	
-	// Support different delimiters 
-	it("should return 10 for '//;\\n1;2,3\n4' string", function(){
+
+	// Support different delimiters
+	it("should return 10 for '//;\\n1;2,3\n4' string", function () {
 		expect(testCalculator.add("//;\n1;2,3\n4")).toEqual(10);
 	})
+
+	// Calling Add with a negative number will throw an exception “negatives not allowed” - and the negative that was passed.
+	// if there are multiple negatives, show all of them in the exception message
+	it("should throw an exception when numbers contain negative number", function () {
+		expect(function () {
+			testCalculator.add("1,-2,-3")
+		}).toThrowError("negatives not allowed: -2,-3");
+	});
 	
 });
